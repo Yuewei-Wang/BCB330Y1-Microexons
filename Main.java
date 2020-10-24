7a8974605e7c449ffb1791eb51a865cdfd8872ea
Cloris Zhou <xiaolin.zhou@mail.utoronto.ca>
2020-10-24 23:29:19 +0800

    Main class returnPercentage()

--- Main.java
+++ Main.java
@@ -15,6 +15,31 @@ public class Main {
         List<Sequence> AllGene = sequenceFile.readBractket(locationFile.getFilepath());
     }
 
+    public double returnPercentage(List<SequenceWithMicroexon> AllGene){
+        int count = 0;
+        for (int i = 0; i < AllGene.size(); i++){
+            SequenceWithMicroexon gene = AllGene.get(i);
+            int A_index = gene.getA_index();
+            int A_length = gene.getA_length();
+            int A_end = A_index + A_length;
+            String seq = gene.getSeq();
+            for (int j = A_index; j < A_end; j++){
+                if (seq.contains("(") || seq.contains(")")){
+                    count ++;
+                }
+            }
+
+            }
+        if (AllGene.size() == 0){
+            throw new IllegalArgumentException("The list has no sequence!");
+        }
+        else {
+            return (double)count / AllGene.size();
+        }
+
+        }
+
+
     public static void main(String[] args) throws IOException {
         String sequences = "/Users/floraw/Documents/HumanNeuronGenes/allC1+A+C2.txt";
         String table = "/Users/floraw/Documents/HumanNeuronGenes/microexon-locations.txt";
