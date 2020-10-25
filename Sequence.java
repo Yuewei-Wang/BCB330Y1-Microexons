3a7d521099f160ceae2536887dc65dd778f0ed2f
wangyuew <yuewei.wang@mail.utoronto.ca>
2020-10-25 13:59:41 -0400

    modified the constructor

--- Sequence.java
+++ Sequence.java
@@ -1,23 +1,10 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Scanner;
-
 public class Sequence {
     private String Name;
     private String Type;
     private String Seq;
     private double Energy;
-    private int A_index;
-    private int A_length;
 
     public Sequence(String title, String seq){
-        String[] titleline = title.split("  ");
         Name = title.substring(title.indexOf("  ")+2,title.lastIndexOf(' '));
         Type = title.substring(title.lastIndexOf(' ')+1);
         Seq = seq;
@@ -40,18 +27,6 @@ public class Sequence {
     public String getType() {
         return Type;
     }
-
-    /**public Sequence(String filepath) throws IOException{
-        DotBracketReader reader = new DotBracketReader(filepath);
-        List<String> collector = reader.readBractket();
-        this.seq = collector.get(0);
-        this.seqTitle = collector.get(2);
-        String energy = collector.get(1);
-        this.energy = Double.parseDouble(energy.substring(1))*(-1);
-    }
-    **/
-
-
 
     public String getSeq() {
         return Seq;
